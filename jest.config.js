c4ba20c609b72e58c24c49fa933a3f26148075fc
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-15 19:18:05 +0100

    chore: :wrench: setup jest and husky

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "<rootDir>/coverage",
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // transform: {
+  //   '.+\\.ts$': 'ts-jest',
+  // },
+};
