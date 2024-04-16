bd01ce03956f2f885caff629ea765c41d02ff77c
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-16 03:22:42 +0100

    chore: :wrench: setup server file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
-import express, {
-  type Express,
-  type NextFunction,
-  type Request,
-  type Response,
-} from "express";
+import express from "express";
+import cors from "cors";
+import routes from "./infrastructure/express/routes";
 
-const app: Express = express();
+const app = express();
 
-app.get("/api", function (_req: Request, res: Response, _next: NextFunction) {
-  res.status(200).send({ message: "Api Okay!" });
-});
+const allowedOrigins = [
+  "http://127.0.0.1:5174",
+  "http://localhost:5174",
+  "http://127.0.0.1:5173",
+  "http://localhost:5173",
+  "http://localhost:3500",
+  "http://localhost:3000",
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+routes(app);
 
 export default app;
