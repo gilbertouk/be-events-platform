1f16c332815752a42b77048a101cfd4733bc0fda
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-16 03:21:05 +0100

    chore: :wrench: update jest and ts config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ["<rootDir>/tests/"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "<rootDir>/coverage",
   coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
   preset: "ts-jest",
   testEnvironment: "node",
-  // transform: {
-  //   '.+\\.ts$': 'ts-jest',
-  // },
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
 };
