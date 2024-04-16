9a4a7a6ca634a2958f9f99749ddaa2125719f035
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-16 13:21:08 +0100

    feat: :sparkles: add notFound http helper

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,10 +1,18 @@
 import { ServerError } from "../errors/server-error";
+import { NotFoundError } from "../errors/not-found-error";
 import { type HttpResponse } from "../protocols/http";
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: {
     message: error.message,
+  },
+});
+
+export const notFound = (): HttpResponse => ({
+  statusCode: 404,
+  body: {
+    error: new NotFoundError().message,
   },
 });
 
