bae64b84f0aa79bf22f2bc30e2480ee7435915ca
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-02-04 18:40:57 +0000

    fix: 🐛 authConfig

--- src/config/serviceAccountKey.ts
+++ src/config/serviceAccountKey.ts
@@ -2,9 +2,7 @@ export const authConfig = {
   type: "service_account",
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY
-    ? JSON.parse(process.env.PRIVATE_KEY)
-    : undefined,
+  private_key: process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : undefined,
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
